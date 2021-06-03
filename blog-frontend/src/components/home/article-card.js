8487c3e8786f5c3ad5ad2f39a0bda1050b50b5c1
Jose Miguel <josyto7@gmail.com>
2021-06-03 21:31:29 +0200

    Fix babel plugin, add fitler to articles

--- blog-frontend/src/components/home/article-card.js
+++ blog-frontend/src/components/home/article-card.js
@@ -3,12 +3,15 @@ import React from "react";
 const ArticleCard = ({ imgSrc, title, description, date }) => (
   <div className="w-3/12 p-4">
     <div className="bg-white rounded-md overflow-hidden shadow-md">
-      <div className="flex justify-center items-center h-40 w-full overflow-hidden">
-        <img src={imgSrc} className="w-full" />
-      </div>
+      <div
+        className="flex justify-center items-center h-40 w-full overflow-hidden bg-cover"
+        style={{ backgroundImage: `url(${imgSrc})` }}
+      ></div>
       <div className="p-4">
-        <h3 className="text-lg font-semibold text-gray-700">{title}</h3>
-        <p className="text-gray-700 text-sm">{description}</p>
+        <h3 className="text-lg font-semibold text-gray-700 truncate-1">
+          {title}
+        </h3>
+        <p className="text-gray-700 text-sm truncate-2">{description}</p>
         <div className="flex justify-end items-center">
           <span className="text-xs">{date}</span>
         </div>
--- blog-frontend/src/pages/Home.js
+++ blog-frontend/src/pages/Home.js
@@ -1,18 +1,35 @@
+// @ts-check
+
 import React, { useEffect, useState } from "react";
 import ArticleCard from "../components/home/article-card";
 
 const Home = () => {
   const [articlesData, setArticlesData] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     callToApi(setArticlesData);
-  }, []);
+  }, []); // [] Indica que sólo se va a realizar cuando se monte el componente!!w
 
   return (
-    <section className="container mx-auto flex justify-start items-center">
-      {articlesData.map((articleData) => (
-        <ArticleCard {...articleData} />
-      ))}
+    <section className="container mx-auto">
+      <div className="p-4">
+        <input
+          className="rounded-lg border border-gray-600 p-4"
+          type="text"
+          onChange={(event) => setSearch(event.target.value.toLowerCase())}
+        />
+      </div>
+      <div className="flex flex-wrap">
+        {articlesData
+          .filter((articleData) => {
+            if (!search) return true;
+            else return articleData.title.toLowerCase().includes(search);
+          })
+          .map((articleData) => (
+            <ArticleCard {...articleData} />
+          ))}
+      </div>
     </section>
   );
 };
