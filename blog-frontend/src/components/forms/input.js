501d38fa410b4cbafde0c2afd626e348c7578cc9
Pablo Castellanos <pablocd168@gmail.com>
2021-06-15 19:18:26 +0200

    Input generic

--- blog-frontend/src/components/forms/input.js
+++ blog-frontend/src/components/forms/input.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const InputText = ({ placeholder, name, className = "" }) => (
+const InputText = ({ placeholder, name, type = "text", className = "" }) => (
   <label
     className={`flex flex-col justify-items-start items-start ${className}`}
   >
     <span className="w-full text-gray-500 ml-2">{placeholder}</span>
     <input
       className="border border-gray-500 rounded-lg px-4 py-2 focus:outline-none focus:border-purple-600"
-      type="text"
+      type={type}
       name={name}
     ></input>
   </label>
