857ae05ee98a0c34e4288abb5e1b1536de2ccb78
Pablo Castellanos <pablocd168@gmail.com>
2021-06-15 19:32:24 +0200

    Simple header

---
+++ blog-frontend/src/components/common/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoIcon from "../icons/logo";
+
+const Header = () => (
+  <header className="w-full h-16 flex justify-between items-center shadow-md px-2">
+    <LogoIcon className="h-10" />
+  </header>
+);
+
+export default Header;
